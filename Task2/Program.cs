8452731ae356785bdfaa7e27c9227a092f0f2190
Alexander <kr.poly@yandex.ru>
2022-08-08 18:36:51 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите любое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите любое число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA == numberB){
+    Console.WriteLine("Эти числа равные друг другу");
+}
+else{
+    if(numberA > numberB){
+    Console.WriteLine($"Максимальное число- {numberA}!");
+    Console.WriteLine($"Минимальое число- {numberB}!");
+    }
+else{
+    Console.WriteLine($"Максимальное число- {numberB}!");
+    Console.WriteLine($"Минимальое число- {numberA}!");   
+}    
+}
