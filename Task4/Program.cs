192aca883614e44e1cf31a05b95fdffe9430081a
Alexander <kr.poly@yandex.ru>
2022-08-09 18:02:30 +0500

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите любое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите любое число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите любое число ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+if(numberB > max){
+    max = numberB;
+}
+if(numberC > max){
+    max = numberC;
+}
+Console.WriteLine(max);
