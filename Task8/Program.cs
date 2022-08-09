aee5eafaa7fa35e3776ed5b4e2f57bfec272395b
Alexander <kr.poly@yandex.ru>
2022-08-09 18:32:33 +0500

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("ВВедите любое число");
+int number= Convert.ToInt32(Console.ReadLine());
+
+for(int i = 2; i <= number; i = i+2)
+{
+    Console.Write(i);
+    Console.Write(", ");
+}
